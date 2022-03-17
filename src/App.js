d00cfaa60e69b10bedc1b77ddfff1718125bee7d
suresh <sureshamahendra.gmail.com>
2022-03-17 17:22:30 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js';
+import ProductSection from './components/ProductSection.js';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productList: []
+    };
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts = async () => {
+    const res = await fetch('https://fakestoreapi.com/products');
+    const json = await res.json();
+    this.setState({
+      productList: json
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ProductSection products={this.state.productList} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
